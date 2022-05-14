f7ef3fe93f25ae2917ca9386005402716f3437cb
Zetta56 <67248032+Zetta56@users.noreply.github.com>
2022-05-14 12:04:32 -0400

    Style tutorial

--- app/src/main/java/com/example/csac/tutorial/PageFragment.kt
+++ app/src/main/java/com/example/csac/tutorial/PageFragment.kt
@@ -1,20 +1,31 @@
 package com.example.csac.tutorial
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.csac.databinding.FragmentTutorialPageBinding
 
-class PageFragment(private val image: Int) : Fragment() {
+class PageFragment(private val image: Int, private val description: Int) : Fragment() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentTutorialPageBinding.inflate(LayoutInflater.from(context))
-        binding.imageView.setImageResource(image)
+        binding.image.setImageResource(image)
+        binding.description.setText(description)
+        binding.description.setOnTouchListener { _, _ ->
+            // Stop view-pager from handling touch event on description
+            binding.root.parent.requestDisallowInterceptTouchEvent(true)
+            false
+        }
+        // Reset description's movement method instead of following view-pager
+        binding.description.movementMethod = ScrollingMovementMethod()
         return binding.root
     }
 }
--- app/src/main/java/com/example/csac/tutorial/TutorialActivity.kt
+++ app/src/main/java/com/example/csac/tutorial/TutorialActivity.kt
@@ -8,24 +8,27 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.csac.R
 import com.example.csac.databinding.ActivityTutorialBinding
-import com.google.android.material.tabs.TabLayoutMediator
 
 class TutorialActivity : AppCompatActivity() {
 
-    private class PagerAdapter(activity: FragmentActivity, private val images: List<Int>) : FragmentStateAdapter(activity) {
+    private class PagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+        val images = listOf(R.drawable.tutorial_one, R.drawable.tutorial_two, R.drawable.tutorial_three)
+        val descriptions = listOf(R.string.tutorial_one, R.string.tutorial_two, R.string.tutorial_three)
+
         override fun getItemCount(): Int {
             return images.size
         }
         override fun createFragment(position: Int): Fragment {
-            return PageFragment(images[position])
+            return PageFragment(images[position], descriptions[position])
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val images = listOf(R.drawable.tutorial_one, R.drawable.tutorial_two, R.drawable.tutorial_three)
+        supportActionBar?.title = "Tutorial"
+
         val binding = ActivityTutorialBinding.inflate(LayoutInflater.from(this))
-        binding.pager.adapter = PagerAdapter(this, images)
+        binding.pager.adapter = PagerAdapter(this)
         setContentView(binding.root)
     }
 
